f4cefdfc6128adcc615df0a3867ebc05a43b613a
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-11-21 20:59:50 -0800

    feat:Added itemsByEndingTime

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,12 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const userKey = (userId: string) => `user#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
-export const itemsKey = (date: string) => `items#${date}`;
+
 export const usernamesUniqueKey = () => `usernames:unique`;
 export const userLikesKey = (userId: string) => `user:likes#${userId}`;
 export const usernamesKey = () => `usernames`;
+
+// Items
+export const itemsKey = (date: string) => `items#${date}`;
+export const itemsByViewsKey = () => `items:views`;
+export const itemsByEndingAtKey = () => `items:endingAt`;
